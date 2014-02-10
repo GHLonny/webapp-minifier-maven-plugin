37cfa68a99e9c488c0dfef6aab34f4101d98e9ea
Lonny <gmljacobson@gmail.com>
2014-02-09 22:03:29 -0700

    Converted test1 POMs to be stand-alone POMs to enable command-line testing with alternate Maven versions.

---
+++ src/test/resources/test1/src/main/java/example/Example.java
@@ -0,0 +1,12 @@
+package example;
+
+/**
+ * This is a placeholder class to ensure that wars are correctly generated.
+ * 
+ * @author Lonny
+ */
+public class Example {
+   public static void main(String[] args) {
+      System.out.println("Hello, world");
+   }
+}
